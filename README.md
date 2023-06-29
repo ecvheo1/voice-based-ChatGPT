90da73b9ef5c79e1a4e0d6651a6d44d1b22676d6
ecvheo1 <ecvheo@naver.com>
2023-06-29 15:30:03 +0900

    create README

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Talkie ChatGPT clone
 
-Talkie is a voice-based ChatGPT clone that can search on the Internet and also in local files
+https://medium.com/better-programming/how-to-create-a-voice-based-chatgpt-clone-that-can-search-on-the-internet-24d7f570ea8#
 
-https://medium.com/better-programming/how-to-create-a-voice-based-chatgpt-clone-that-can-search-on-the-internet-24d7f570ea8
+
+# 실행 방법
+```
+export OPENAI_API_KEY=''
+export SERPER_API_KEY=‘’
+python app.py
+```
--- server.py
+++ server.py
@@ -2,7 +2,8 @@ import os
 import whisper
 from flask import Flask, render_template, request
 from chains import get_chat_chain, get_search_agent, get_qa_chain
-import pyttsx3
+import math
+import time
 
 # get path for static files
 static_dir = os.path.join(os.path.dirname(__file__), 'static')  
@@ -13,7 +14,7 @@ if not os.path.exists(static_dir):
 audio_file = 'recording.webm'
 
 # load whisper model
-model = whisper.load_model('small.en')
+model = whisper.load_model('large-v2')
 
 chat_chain = get_chat_chain()
 search_agent = get_search_agent()
@@ -31,11 +32,18 @@ def record():
     # get file from request and save it
     file = request.files['audio']
     file.save(audio_file)
+    
+    stt_start = time.time()
 
     # transcribe the audio file using Whisper and extract the text
     audio = whisper.load_audio(audio_file) 
-    result = model.transcribe(audio)
+    result = model.transcribe(audio, fp16=False)
     text = result["text"]
+    
+    math.factorial(100000)
+    stt_end = time.time()
+    print(text)
+    print("걸린 시간" + " " + f"{stt_end - stt_start:.5f} sec")
 
     # remove the temp audio file
     if os.path.exists(audio_file):
@@ -45,16 +53,9 @@ def record():
     # output = chat_chain.predict(human_input=text)
     # output = search_agent.run(input=text)
     output = qa_chain.run(text)
-    
-    # say out the response
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 190)
-    engine.setProperty('voice', os.environ['VOICE'])
-    engine.say(output)
-    engine.startLoop()
 
     # remove the temp audio file
     if os.path.exists(audio_file):
-        os.remove(audio_file)    
-    
+        os.remove(audio_file)
+        
     return {"input": text, "output": output.replace("\n", "<br />")}
